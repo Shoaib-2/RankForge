652ee5e622ca276c83852dbe6f60b0306d32569c
Mohammed Shoaib <shoaibkkhn50665@gmail.com>
2025-01-22 22:51:08 +0000

    Changed client variables.

--- server/server.js
+++ server/server.js
@@ -45,6 +45,8 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   }
 });
 
